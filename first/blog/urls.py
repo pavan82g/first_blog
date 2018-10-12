c9d7f51733e97dfeb613f96332a8328f63bbbee6
SRM-Hackathon/DopeCode <pavan.chetty82g@gmail.com>
2018-10-12 18:07:50 +0530

    TEST COMMIT

--- first/blog/urls.py
+++ first/blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path 
+from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list1'),
+    path('next/', views.button, name="button")
 ]
--- first/blog/views.py
+++ first/blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+import subprocess
+from blog.python1 import sum
+from django.template.context_processors import csrf
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/index.html', {'posts':posts}) 
+
+def button(request):
+    if request.method == 'POST':
+        #subprocess.check_call(['python', 'python1.py'])
+        val = sum()
+        print(val)
+    return render(request, "blog/file.html", {"val":val})    
